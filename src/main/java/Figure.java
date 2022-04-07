8eaa76409c48472665e00f6061be3f2352a31851
Joana Vilas Boas <joana.gvb@gmail.com>
2022-04-07 16:52:45 +0100

    Implementei get e set da classe figura

--- src/main/java/Figure.java
+++ src/main/java/Figure.java
@@ -3,4 +3,11 @@ public abstract class Figure {
 
         private Color color;
 
+        public Color getColor() {
+                return color;
+        }
+
+        public void setColor(Color color) {
+                this.color = color;
+        }
 }
