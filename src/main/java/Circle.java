e2fcaaf61fdeffec8a9bd28168a6b522da34ccd8
Flavio Gravato <flavio.gravato@gmail.com>
2022-04-07 16:18:04 +0100

    Added Circle Class
    Missing Expand method

---
+++ src/main/java/Circle.java
@@ -0,0 +1,17 @@
+public class Circle {
+    private Point center;
+    private double radius;
+
+    private static final double RADIUS_DEFAULT = 0.0;
+
+    public Circle(Point center, double radius) {
+        this.center = center;
+        this.radius = radius;
+    }
+
+    public Circle() {
+        this.center=new Point();
+        this.radius=RADIUS_DEFAULT;
+    }
+    
+}
