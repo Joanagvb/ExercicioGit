10694fe384a35a803b5c601472df02d5b67d0f7d
Flavio Gravato <flavio.gravato@gmail.com>
2022-04-07 17:03:38 +0100

    Lots of shit

--- src/main/java/Figure.java
+++ src/main/java/Figure.java
@@ -4,14 +4,10 @@ public abstract class Figure {
         private Color color;
 
 
-<<<<<<< HEAD
-<<<<<<< HEAD
         public abstract double area();
 
-=======
         public abstract double perimeter();
->>>>>>> 76b18b9cce5a16497b353e50254204bbc4f5c418
-=======
+
         public Color getColor() {
                 return color;
         }
@@ -19,9 +15,5 @@ public abstract class Figure {
         public void setColor(Color color) {
                 this.color = color;
         }
-
-
-        public abstract double perimeter();
-
->>>>>>> 549a00c8a522eeefb7a1278763e6aee406e168f8
+        
 }
