b29a2c98bb5415811392ab24969e0d81d6944b68
Joana Vilas Boas <joana.gvb@gmail.com>
2022-04-08 08:33:17 +0100

    Actualizacao Classe Color

--- src/main/java/Color.java
+++ src/main/java/Color.java
@@ -2,6 +2,43 @@
 
 public class Color {
 
+    public enum Cores {
+        BRANCO {
+            @Override
+            public String toString() {
+                return "Branco";
+            }
+        },
+        AMARELO {
+            @Override
+            public String toString() {
+                return "Amarelo";
+            }
+        },
+        AZUL {
+            @Override
+            public String toString() {
+                return "Azul";
+            }
+        },
+        VERMELHO {
+            @Override
+            public String toString() {
+                return "Vermelho";
+            }
+        },
+        VERDE {
+            @Override
+            public String toString() {
+                return "Verde";
+            }
+        },
+        PRETO {
+            @Override
+            public String toString() {
+                return "Preto";
+            }
+        };
 
 
-}
+}}
